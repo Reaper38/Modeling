418ca59d182155fd243398594c308a686648b6a5
reaper38 <spiritgame38@gmail.com>
2015-10-16 00:08:31 +0300

    added first lab

---
+++ RandomChecker/RandomChecker/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace RandomChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SingleDigit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DoubleDigit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TripleDigit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.UserBased = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SingleDigit,
+            this.DoubleDigit,
+            this.TripleDigit});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 80;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(470, 398);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // SingleDigit
+            // 
+            this.SingleDigit.HeaderText = "0 <= X < 10";
+            this.SingleDigit.MaxInputLength = 1;
+            this.SingleDigit.Name = "SingleDigit";
+            this.SingleDigit.ReadOnly = true;
+            // 
+            // DoubleDigit
+            // 
+            this.DoubleDigit.HeaderText = "0 <= X < 100";
+            this.DoubleDigit.MaxInputLength = 2;
+            this.DoubleDigit.Name = "DoubleDigit";
+            this.DoubleDigit.ReadOnly = true;
+            // 
+            // TripleDigit
+            // 
+            this.TripleDigit.HeaderText = "0 <= X < 1000";
+            this.TripleDigit.MaxInputLength = 3;
+            this.TripleDigit.Name = "TripleDigit";
+            this.TripleDigit.ReadOnly = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserBased});
+            this.dataGridView2.Location = new System.Drawing.Point(978, 32);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(124, 398);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellEndEdit);
+            this.dataGridView2.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView2_UserDeletedRow);
+            // 
+            // UserBased
+            // 
+            this.UserBased.HeaderText = "Any X";
+            this.UserBased.Name = "UserBased";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AllowUserToResizeColumns = false;
+            this.dataGridView3.AllowUserToResizeRows = false;
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dataGridView3.Location = new System.Drawing.Point(494, 32);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.RowHeadersWidth = 80;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(470, 398);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "0 <= X < 10";
+            this.dataGridViewTextBoxColumn1.MaxInputLength = 1;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "0 <= X < 100";
+            this.dataGridViewTextBoxColumn2.MaxInputLength = 2;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "0 <= X < 1000";
+            this.dataGridViewTextBoxColumn3.MaxInputLength = 3;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Generated sequence";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(494, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Table sequence";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(978, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Input";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 438);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(178, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(334, 438);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(504, 438);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(660, 438);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 17);
+            this.label8.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(816, 438);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(988, 438);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 17);
+            this.label10.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 463);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Random checker";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SingleDigit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DoubleDigit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TripleDigit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserBased;
+    }
+}
+
---
+++ RandomChecker/RandomChecker/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomChecker
+{
+    public partial class Form1 : Form
+    {
+        // Array size
+        const int ACount = 1000;
+
+        // Shown number of arrays
+        const int NCount = 15;
+
+        // Sequence limit
+        const int Lim = 100;
+
+        // Machine-randomized numbers
+        int[] SDRand;
+        int[] DDRand;
+        int[] TDRand;
+
+        // Numbers from textfile
+        int[] SDFile;
+        int[] DDFile;
+        int[] TDFile;
+
+        // Get all numbers from textfile to int array
+        static int[] ParseIntFile(string FName)
+        {
+            string FText = File.ReadAllText(FName);
+
+            string[] IStr = FText.Split(new char[] { '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int[] Result = new int[IStr.Length];
+
+            for (int I = 0; I < IStr.Length; ++I)
+                Result[I] = int.Parse(IStr[I]);
+
+            return Result;
+        }
+
+        // Get evaluation of randomized sequence
+        public double RandomCheck(ref int[] Sequence)
+        {
+            // If sequence is too big
+            if (Sequence.Count() > Lim)
+            {
+                double Step = (double)Sequence.Count() / (double)Lim;
+
+                int[] Seq = new int[Lim];
+                for (int I = 0; I < Lim; ++I)
+                    Seq[I] = Sequence[(int)(Step * I)];
+
+                return RandomCheck(ref Seq);
+            }
+
+            // Calculating pyramid size
+            int Count = 1;
+            for (int I = Sequence.Count() - 1; I > 1; --I)
+                Count += I;
+
+            // Pyramid of subsequent substractions
+
+            int[] Pyramid = new int[Count];
+
+            for (int I = 1; I < Sequence.Count(); ++I)
+                Pyramid[I - 1] = Sequence[I] - Sequence[I - 1];
+
+            int J = 0;
+            for (int I = Sequence.Count() - 1; I > 0; --I)
+            {
+                for (int K = 1; K < I; ++K)
+                {
+                    Pyramid[J + I + K - 1] = Pyramid[J + K] - Pyramid[J + K - 1];
+                }
+
+                J += I;
+            }
+
+            // Get all distinct numbers (bar 0) from pyramid
+            int[] Diff = Pyramid.Distinct().Where(X => X != 0).ToArray();
+
+            return (double)(Diff.Count()) / (double)(Pyramid.Count());
+        }
+
+        // Evaluation of user input
+        private void CalculateGrid()
+        {
+            // We can evaluate sequence of three and more numbers
+            int RCount = dataGridView2.RowCount - 1;
+            if (RCount < 3)
+            {
+                label10.Text = "More numbers!";
+                return;
+            }
+
+            // Convert user input to number array
+            int[] Nums = new int[RCount];
+            for (int I = 0; I < RCount; ++I)
+            {
+                try
+                {
+                    Nums[I] = Convert.ToInt32(dataGridView2[0, I].Value);
+                }
+                catch (FormatException)
+                {
+                    label10.Text = "Error: row " + (I + 1).ToString() + "!";
+                    return;
+                }
+            }
+
+            // Array evaluation
+            double Res = RandomCheck(ref Nums);
+            label10.Text = "PRB = " + Res.ToString("0.0000%");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Evaluation of machine-randomized and textfile numbers
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Random Rand = new Random();
+
+            // Generating machine-randomized numbers
+
+            SDRand = new int[ACount];
+            DDRand = new int[ACount];
+            TDRand = new int[ACount];
+
+            for (int I = 0; I < ACount; ++I)
+            {
+                SDRand[I] = Rand.Next(0, 10);
+                DDRand[I] = Rand.Next(0, 100);
+                TDRand[I] = Rand.Next(0, 1000);
+            }
+
+            // Loading numbers from textfile
+
+            SDFile = ParseIntFile("single_digits.txt");
+            DDFile = ParseIntFile("double_digits.txt");
+            TDFile = ParseIntFile("triple_digits.txt");
+
+            for (int I = 0; I < NCount; ++I)
+            {
+                dataGridView1.Rows.Add(SDRand[I], DDRand[I], TDRand[I]);
+                dataGridView3.Rows.Add(SDFile[I], DDFile[I], TDFile[I]);
+            }
+
+            // Evaluating machine-randomized and textfile numbers
+
+            double Res = RandomCheck(ref SDRand);
+            label4.Text = "PRB = " + Res.ToString("0.0000%");
+
+            Res = RandomCheck(ref DDRand);
+            label5.Text = "PRB = " + Res.ToString("0.0000%");
+
+            Res = RandomCheck(ref TDRand);
+            label6.Text = "PRB = " + Res.ToString("0.0000%");
+
+            Res = RandomCheck(ref SDFile);
+            label7.Text = "PRB = " + Res.ToString("0.0000%");
+
+            Res = RandomCheck(ref DDFile);
+            label8.Text = "PRB = " + Res.ToString("0.0000%");
+
+            Res = RandomCheck(ref TDFile);
+            label9.Text = "PRB = " + Res.ToString("0.0000%");
+
+            CalculateGrid();
+        }
+
+        // On cell edit
+        private void dataGridView2_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            CalculateGrid();
+        }
+
+        // On row deletion
+        private void dataGridView2_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            CalculateGrid();
+        }
+    }
+}
