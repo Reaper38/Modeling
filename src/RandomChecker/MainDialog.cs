55015f603a4d10f71b095621ca8ebbf069452748
nitrocaster <paul-kv@yandex.ru>
2015-10-16 10:14:51 +0300

    Clean dumb comments.

--- src/RandomChecker/MainDialog.cs
+++ src/RandomChecker/MainDialog.cs
@@ -15,8 +15,7 @@ namespace RandomChecker
         // Numbers from textfile
         private int[] sdFile, ddFile, tdFile;
         private readonly IRandomSequenceTest rndTest = new PyramidTest();
-
-        // Get all numbers from textfile to int array
+        
         private static int[] ParseIntFile(string fName)
         {
             string fText = File.ReadAllText(fName);
@@ -26,9 +25,7 @@ namespace RandomChecker
                 result[i] = int.Parse(str[i]);
             return result;
         }
-        // Get evaluation of randomized sequence
         
-        // Evaluation of user input
         private void CalculateGrid()
         {
             // We can evaluate sequence of three and more numbers
@@ -59,11 +56,10 @@ namespace RandomChecker
         {
             InitializeComponent();
         }
-        // Evaluation of machine-randomized and textfile numbers
+        
         private void MainDialog_Shown(object sender, EventArgs e)
         {
             Random rand = new Random();
-            // Generating machine-randomized numbers
             sdRand = new int[ACount];
             ddRand = new int[ACount];
             tdRand = new int[ACount];
@@ -73,7 +69,6 @@ namespace RandomChecker
                 ddRand[i] = rand.Next(0, 100);
                 tdRand[i] = rand.Next(0, 1000);
             }
-            // Loading numbers from textfile
             sdFile = ParseIntFile("single_digits.txt");
             ddFile = ParseIntFile("double_digits.txt");
             tdFile = ParseIntFile("triple_digits.txt");
@@ -82,7 +77,6 @@ namespace RandomChecker
                 dataGridView1.Rows.Add(sdRand[i], ddRand[i], tdRand[i]);
                 dataGridView3.Rows.Add(sdFile[i], ddFile[i], tdFile[i]);
             }
-            // Evaluating machine-randomized and textfile numbers
             PerformTest(label4, sdRand);
             PerformTest(label5, ddRand);
             PerformTest(label6, tdRand);
@@ -97,10 +91,10 @@ namespace RandomChecker
             var result = rndTest.Test(src, Lim)[0];
             l.Text = string.Format("PRB = {0}%", result.ToString("0.0000"));
         }
-        // On cell edit
+
         private void dataGridView2_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         { CalculateGrid(); }
-        // On row deletion
+
         private void dataGridView2_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
         { CalculateGrid(); }
     }
